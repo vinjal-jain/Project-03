85853ad5a3d8f0ebe7fa88c2bc1c410f9206dc88
vinjal-jain <vinjaljain14it@gmail.com>
2025-04-24 15:21:21 +0530

    done

---
+++ project_3/src/main/java/in/co/rays/project_3/dto/TransportationDTO.java
@@ -0,0 +1,50 @@
+package in.co.rays.project_3.dto;
+
+import java.util.Date;
+
+
+
+public class TransportationDTO extends BaseDTO {
+	 
+	 private String description ;
+	 private String mode ;
+	 private Date date  ;
+	 private Integer cost ;
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getMode() {
+		return mode;
+	}
+	public void setMode(String mode) {
+		this.mode = mode;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public Integer getCost() {
+		return cost;
+	}
+	public void setCost(Integer cost) {
+		this.cost = cost;
+	}
+	@Override
+	public String getKey() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public String getValue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	 
+	 
+
+}
---
+++ project_3/src/main/java/in/co/rays/project_3/model/TransportationModelInt.java
@@ -0,0 +1,24 @@
+package in.co.rays.project_3.model;
+
+import java.util.List;
+
+import in.co.rays.project_3.dto.TransportationDTO;
+import in.co.rays.project_3.exception.ApplicationException;
+import in.co.rays.project_3.exception.DuplicateRecordException;
+
+public interface TransportationModelInt {
+ 
+	 
+	 
+	public long add(TransportationDTO dto)throws ApplicationException,DuplicateRecordException;
+	public void delete(TransportationDTO dto)throws ApplicationException;
+	public void update(TransportationDTO dto)throws ApplicationException,DuplicateRecordException;
+	public TransportationDTO findByPK(long pk)throws ApplicationException;
+	public TransportationDTO findByLogin(String login)throws ApplicationException;
+	public List list()throws ApplicationException;
+	public List list(int pageNo,int pageSize)throws ApplicationException;
+	public List search(TransportationDTO dto,int pageNo,int pageSize)throws ApplicationException;
+	public List search(TransportationDTO dto)throws ApplicationException;
+	public List getRoles(TransportationDTO dto)throws ApplicationException;
+
+}
